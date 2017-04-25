6c94113a305c92be0105d8cc9e508f9cd133a11f
Chema Alonso Josa <nimiux@users.noreply.github.com>
2017-04-25 16:39:27 +0200

    Update front-end.js
    Use partial function applicarion

--- ut4/js/front-end.js
+++ ut4/js/front-end.js
@@ -13,6 +13,9 @@ function $(id) {
     return document.getElementById(id);
 }
 
+// Try:
+var $ = document.getElementById.bind(document);
+
 front.generateDate = function () {
     "use strict";
     $("fecha").value = back.getDate();
